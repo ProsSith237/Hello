2bb2a2535d38692f75e769fa6ad3196aee6eae08
ProsSith237 <165108976+ProsSith237@users.noreply.github.com>
2024-10-27 21:00:24 +0700

    Add files via upload

---
+++ nano hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# This is a single-line comment in Python.
+# It's used to explain what the code does or provide additional information.
+
+# The print() function is used to output text to the console.
+# In this case, we're using it to print "Hello, World!"
+print("Hello, World!")
